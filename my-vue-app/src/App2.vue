c92391c96a95fe5c334e15823018cfd6d786a187
ChocolateAceCream <nuodi@hotmail.com>
2021-02-18 15:28:45 +0800

    FEAT: add sample code for koa and vite.config

--- my-vue-app/src/App2.vue
+++ my-vue-app/src/App2.vue
@@ -1,10 +1,17 @@
 <template>
-  <h1>hey </h1>
+  <h1>hey {{}}</h1>
 </template>
 
 
 <script>
+import { provide } from 'vue'
 export default {
-  name: 'App2'
+  name: 'App2',
+  inject: ['i18n'],
+  computed: {
+    translate() {
+      return this.$translate
+    }
+  }
 }
 </script>
--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -3,9 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import App2 from './App2.vue'
 
+import i18nPlugin  from './plugins/i18n'
 createApp(App).mount('#app')
 
 const app2Mixin = {
+  custom: 'app 2!',
   created() {
     console.log('app 2 created')
   },
@@ -14,7 +16,47 @@ const app2Mixin = {
   }
 }
 
-const app2 = createApp(App2, {mixins: [app2Mixin]})
-// app2.mixin({...app2Mixin}) 
+const app3Mixin = {
+  custom: 'app 3!',
+  created() {
+    console.log('app 3 created')
+  },
+  mounted() {
+    console.log('app 3 mounted')
+  }
+}
+
+
+
+const app2 = createApp(App2)
+
+//merging strategies
+app2.config.optionMergeStrategies.custom = (toVal, fromVal) => {
+  console.log('from', fromVal)
+  console.log('toVal', toVal)
+  // => "goodbye!", undefined
+  // => "hello", "goodbye!"
+  return fromVal || toVal
+}
+
+//inject global mixin, which will affect all child components
+app2.mixin({...app2Mixin}) 
+app2.mixin({...app3Mixin}) 
+app2.mixin({
+  custom: 'goodbye!',
+  created() {
+    console.log(this.$options.custom) // => "hello!"
+    console.log('process', this.process)
+}
+})
+
+//use plugin
+const i18nStrings = {
+  greetings: {
+    hi: 'Hallo!'
+  }
+}
+
+app2.use(i18nPlugin ,i18nStrings)
 app2.mount('#app2')
 
