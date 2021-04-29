9d9d27f264b334a494878d5f5b521e00d66624d4
ChocolateAceCream <nuodi@hotmail.com>
2021-04-29 17:19:15 +0800

    FEAT: add dynamic render() example

--- faults/src/components/RenderInSetupExample.js
+++ faults/src/components/RenderInSetupExample.js
@@ -1,12 +1,33 @@
 import { h, resolveComponent, resolveDirective, withDirectives,reactive } from 'vue'
 export default {
+  //this render function will be override by the one from setup()
+  render() {
+    return h('div', {}, [
+      'some text come first',
+      h('h1', 'a hdeadline')
+    ])
+
+    // return h('div',
+    //   Array.from({ length: 20 }).map(() => {
+    //     return h('p', 'hi')
+    //   })
+    // )
+
+  },
   setup() {
-    const state = reactive({count:0})
+    const state = reactive({
+      count: 0,
+      arr: []
+    })
     function increment() {
       state.count++
+      state.arr.push(h('p', {}, state.count))
     }
     return () => {
-      return h('div', {onClick: increment}, state.count)
+      return h('div', { onClick: increment }, [
+        state.count,
+        h('div', { style: { display: "flex" } }, [...state.arr])
+      ])
     }
   }
 }
